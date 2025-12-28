3da37388ab0284e7c574c09ac0c9a805aac19144
billfdkfdk-source <billfdkfdk@gmail.com>
2025-12-28 20:18:57 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // 确保你根目录下有一个 App.tsx 文件
-import './index.css'
+// 假设你的主代码在 index.tsx 或者 App.tsx 里，请确保文件名对应
+// 如果你把所有 UI 代码都写在了 index.tsx，请用下面这行：
+import App from './index.tsx' 
 
-// 找到 index.html 里的那个 <div id="root"></div>
 const rootElement = document.getElementById('root');
-
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   )
-} else {
-  console.error("找不到 root 节点，请检查 index.html 中是否有 <div id='root'></div>");
 }
