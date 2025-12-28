5f17ee9a1de035559ed20109ce6e9d4d7c54ba17
billfdkfdk-source <billfdkfdk@gmail.com>
2025-12-28 19:46:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 解决 index.css 404 的核心
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'), // 保持你的路径别名一致
+    },
+  },
+})
